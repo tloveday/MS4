56066647e87ba944703e93847dd70ae492a0496b
tloveday <thomas_loveday848@hotmail.com>
2022-01-18 14:21:15 +0000

    Added basket context and logic

---
+++ basket/contexts.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+
+
+def basket_contents(request):
+
+    basket_items = []
+    total = 0
+    products_count = 0
+
+    grand_total = total
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'products_count': products_count,
+        'grand_total': grand_total,
+    }
+
+    return context
--- teifitkd/settings.py
+++ teifitkd/settings.py
@@ -69,9 +69,10 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # Necessary do not remove
+                'django.template.context_processors.request',  # Necessary for allauth not remove
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
