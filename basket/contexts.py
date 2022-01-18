2c2aa73fd76919c221717ceb4da2925336066223
tloveday <thomas_loveday848@hotmail.com>
2022-01-18 15:06:55 +0000

    Addition of functionality to add products to basket

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 
 
 def basket_contents(request):
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ from django.shortcuts import render
 def view_basket(request):
     """ View that brings the basket contents page """
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of product to the shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
+
