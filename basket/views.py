9120050b7b81713d2ee08445182a4c0ad848d5a4
tloveday <thomas_loveday848@hotmail.com>
2022-01-21 21:05:56 +0000

    Added toasts

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +14,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of product to the  basket """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -31,6 +35,7 @@ def add_to_basket(request, item_id):
             basket[item_id] += quantity
         else:
             basket[item_id] = quantity
+            messages.success(request, f'Added {product.name} to the basket')
 
     request.session['basket'] = basket
 
