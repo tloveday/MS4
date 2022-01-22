6df535d4385a2a033a60d363e9a4cdc048abb553
tloveday <thomas_loveday848@hotmail.com>
2022-01-22 15:32:46 +0000

    Added CSS for notifications, basket preview and other messages.

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -14,7 +14,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of product to the  basket """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -26,13 +26,17 @@ def add_to_basket(request, item_id):
         if item_id in list(basket.keys()):
             if size in basket[item_id]['items_by_size'].keys():
                 basket[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {basket[item_id]["items_by_size"][size]}')
             else:
                 basket[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upoer()} {product.name} to the basket')
         else:
             basket[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size.upoer()} {product.name} to the basket')
     else:
         if item_id in list(basket.keys()):
             basket[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
         else:
             basket[item_id] = quantity
             messages.success(request, f'Added {product.name} to the basket')
@@ -45,6 +49,7 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """ Add the quantity of product to the basket to the amount specified """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
@@ -54,15 +59,19 @@ def adjust_basket(request, item_id):
     if size:
         if quantity > 0:
             basket[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {basket[item_id]["items_by_size"][size]}')
         else:
             del basket[item_id]['items_by_size'][size]
             if not basket[item_id]['items_by_size']:
                 basket.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from the basket')
     else:
         if quantity > 0:
             basket[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
         else:
             basket.pop(item_id)
+            messages.success(request, f'Removed {product.name} from the basket')
 
     request.session['basket'] = basket
 
@@ -72,6 +81,7 @@ def adjust_basket(request, item_id):
 def remove_from_basket(request, item_id):
     """ Remove the product from the basket """
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -81,12 +91,15 @@ def remove_from_basket(request, item_id):
             del basket[item_id]['items_by_size'][size]
             if not basket[item_id]['items_by_size']:
                 basket.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from the basket')
         else:
             basket.pop(item_id)
+            messages.success(request, f'Removed {product.name} from the basket')
 
         request.session['basket'] = basket
 
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
