b877bf07c5add3322d7c75883741439cf65f4621
tloveday <thomas_loveday848@hotmail.com>
2022-01-23 23:05:03 +0000

    Stripe installed elements set up and checkout css  added.

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,47 @@
+/*
+    Core logic comes from 
+    https://stripe.com/docs/js
+
+    https://stripe.com/docs/payments/quickstart
+
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#f3101f',
+        iconColor: '#f3101f'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+
+// Readtime validation of errors
+// Validation Errors on Card
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+            <i class="fas fa-times-circle"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
 
-# Create your views here.
+import stripe
 
 
 def checkout(request):
@@ -12,10 +14,16 @@ def checkout(request):
         messages.error(request, "Your basket is empty")
         return redirect(reverse('products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KLDkbJhG3huTzYi2745itzYf8LU9ct5BTtR6Hx36zuogP3sOhL2oZhsWxhrrO57f0zWa5efHaXMFxcdedL8Gwme004R7qziVH',
+        'client_secret': 'test',
     }
 
     return render(request, template, context)
