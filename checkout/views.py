4abd9b94939ebeec6bf5ff597b8aa41a901f5bc7
tloveday <thomas_loveday848@hotmail.com>
2022-01-24 10:02:55 +0000

    Added Stripe payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "Your basket is empty")
@@ -17,13 +20,24 @@ def checkout(request):
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+    print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public ket is missing - Did you set it')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KLDkbJhG3huTzYi2745itzYf8LU9ct5BTtR6Hx36zuogP3sOhL2oZhsWxhrrO57f0zWa5efHaXMFxcdedL8Gwme004R7qziVH',
-        'client_secret': 'test',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
