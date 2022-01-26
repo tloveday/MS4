a2e440f61afe48bd8faf7d0d02e57dbd92784543
tloveday <thomas_loveday848@hotmail.com>
2022-01-26 21:41:57 +0000

    Addition of the Ability to delete products

--- products/views.py
+++ products/views.py
@@ -72,9 +72,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product successfully added')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product.')
     else:
@@ -94,7 +94,7 @@ def edit_product(request, product_id):
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
-            message.success(request, 'Product successfully updated')
+            messages.success(request, 'Product successfully updated')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to update product')
@@ -109,5 +109,14 @@ def edit_product(request, product_id):
         }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    # delete Product in the Store
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product Deleted')
+
+    return redirect(reverse('products'))
 
 
