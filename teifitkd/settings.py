c961ce4bcd2e558744ba33013e3135d2961a1854
tloveday <thomas_loveday848@hotmail.com>
2022-01-28 13:15:22 +0000

    Removed Secret key

--- teifitkd/settings.py
+++ teifitkd/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$v0_g4lj$0wp$n#120q&8b4!9hi&*gwm@)de@emj^p)ol4b@k3'
+SECRET_KEY = os.environget('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
