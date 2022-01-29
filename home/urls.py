32f72d3f6dd0bc1c67c41abde805e2ba720adbfc
tloveday <thomas_loveday848@hotmail.com>
2022-01-29 20:30:35 +0000

    Addition of Our Team Page and content on Class pages, alongside other image files

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('our_team', views.our_team, name='our_team'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     """ View for index page"""
     return render(request, 'home/index.html')
+
+
+def our_team(request):
+    """ View for index page"""
+    return render(request, 'home/our_team.html')
