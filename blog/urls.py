059c426ded0a2da533f7fdf227140b103fc745ae
tloveday <thomas_loveday848@hotmail.com>
2022-02-27 23:48:30 +0000

    Addition of blog views for blog home, post detail and add post, alongside urls and html.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.blog, name='blog')
+    path('', views.blog, name='blog'),
+    path('<int:post_id>/', views.blog_post, name='post'),
+    path('addpost/', views.add_post, name='add_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
-from django.shortcuts import render
-
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    get_object_or_404
+)
+from django.contrib import messages
 from .models import Post
 from .forms import PostForm
 
@@ -9,4 +14,45 @@ from .forms import PostForm
 
 def blog(request):
     """ Returns blog.html """
-    return render(request, 'blog/blog.html', {})
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog.html', context)
+
+
+def blog_post(request, post_id):
+    """ Returns post.html """
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post.html', context)
+
+
+def add_post(request):
+    # Superuser Only
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, You do not have permission to do that')
+        return redirect(reverse('blog'))
+    # Add Blog Post
+    if request.method == 'POST':
+        post_form = PostForm(request.POST, request.FILES)
+        if post_form.is_valid():
+            post = post_form.save()
+            messages.success(request, 'Blog post successfully published')
+            return redirect(reverse('post', args=[post.id]))
+        else:
+            messages.error(request, 'Failed to add post.')
+    else:
+        post_form = PostForm()
+
+    template = 'blog/add_post.html'
+    context = {
+        'post_form': post_form,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
